19a10c5607ca6ee1bf8434fff24688ec62721ada
BhaskarPotti <potti_bhaskar@yahoo.com>
2021-03-27 23:32:35 -0400

    Add files via upload

---
+++ terraform_phase_one/main.tf
@@ -0,0 +1,18 @@
+# Setting "aws_region" to an undefined Terraform variable forces a user to have to
+# manually enter in their preferred AWS region each time when using "terraform plan"
+# or "terraform apply" commands. The advantage of this slight inconvenience prevents
+# users from accidently creating services outside of their preferred region.
+
+variable "aws_region" {
+  description = "Enter in a value for aws-region (e.g. us-east-1, us-west-2, etc.)"
+  type = string
+}
+
+# There are no needs for access_key and secret_key inputs if your AWS credential are
+# stored in their default location utilized in other AWS terminal controlled services
+# (e.g. awscli). Terraform should automatically fetch the correct authentication
+# information.
+
+provider "aws" {
+  region = var.aws_region
+}
